6c8a37bb0f58be9df74fcd05f84f2a2779cf4203
David Umphress <umphress@eng.auburn.edu>
2021-08-16 17:16:01 -0500

    Fix return type on test 100

--- Assignment/collapseTest.py
+++ Assignment/collapseTest.py
@@ -7,7 +7,7 @@ class CollapseTest(unittest.TestCase):
 # Happy path test
     def test100_100_ShouldCollapseSingleDigit(self):
         value = '5'
-        expectedResult = 5
+        expectedResult = '5'
         actualResult = collapse.collapse(value)
         self.assertEqual(expectedResult, actualResult)
     
